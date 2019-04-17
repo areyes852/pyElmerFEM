68b4c7b0987393bd370ef1396ce376058a1e8536
Alberto <a_reyes@outlook.es>
2019-04-17 12:32:06 +0200

    Añádense archivos de muestra (en subcarpeta "muestras"). Escríbese unha función que lee os archivos .vtu e devolve as propiedades dos elementos.

--- src/pyElmerFEM.py
+++ src/pyElmerFEM.py
@@ -1,4 +1,50 @@
 # -*- coding: utf-8 -*-
 
+import vtk
+
+def leer_vtu(archivo):
+    reader = vtk.vtkXMLUnstructuredGridReader()
+    reader.SetFileName(archivo)
+    reader.Update()  # Needed because of GetScalarRange
+    output = reader.GetOutput().GetPointData()
+    props = []
+    p = []
+    propiedad = ''
+    bol = 0
+    # Só hai iguales nas liñas que inclúen o nome da propiedade.
+    # Se atopo un = añado os carácteres á propiedade ata atopar un salto de liña
+    for c in str(output):
+        if c == '\n' and bol == 1:
+            bol = 0
+            propiedad = ''.join(p).strip()
+            props.append(propiedad)
+            p = []
+        if bol == 1:
+            p.append(c)
+        if c == '=':
+            bol = 1
+
+    for i in props:
+        a = output.GetScalars(i)
+        print(a)
+    print(props)
+
+def leer_elements():
+    # Código
+    print()
+
+def leer_boundary():
+    # Código
+    print()
+
+def leer_header():
+    # Código
+    print()
+
+def leer_nodes():
+    # Código
+    print()
 
 if __name__ == "__main__":
+    archivo = "../muestras/case0001.vtu"
+    leer_vtu(archivo)

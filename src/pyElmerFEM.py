82d4d8581323a741d5c0116c054e9064f397c701
Alberto <a_reyes@outlook.es>
2019-04-17 15:07:05 +0200

    Escritas unha función para obter os datos binarios do archivo vtu (obter_raw_vtu(archivo)) e outra función para obter a posición (offset) na que empezan os datos para cada propiedade dentro do binario obtido ca funcion anterior (bin_offset_prop(output,propiedades))

--- src/pyElmerFEM.py
+++ src/pyElmerFEM.py
@@ -2,17 +2,21 @@
 
 import vtk
 
-def leer_vtu(archivo):
+def abrir_vtu(archivo):
     reader = vtk.vtkXMLUnstructuredGridReader()
     reader.SetFileName(archivo)
     reader.Update()  # Needed because of GetScalarRange
-    output = reader.GetOutput().GetPointData()
+    return reader.GetOutput().GetPointData()
+
+
+def lista_vtu_props(output):
     props = []
     p = []
     propiedad = ''
     bol = 0
     # Só hai iguales nas liñas que inclúen o nome da propiedade.
     # Se atopo un = añado os carácteres á propiedade ata atopar un salto de liña
+
     for c in str(output):
         if c == '\n' and bol == 1:
             bol = 0
@@ -23,11 +27,44 @@ def leer_vtu(archivo):
             p.append(c)
         if c == '=':
             bol = 1
+    return props
 
-    for i in props:
-        a = output.GetScalars(i)
-        print(a)
-    print(props)
+
+def bin_offset_prop(output,propiedades):
+    offset_anterior = 0
+    offsets = [0]
+    for propiedade in propiedades:
+        tamaño = output.GetArray(propiedade).GetSize()
+        tipo = output.GetArray(propiedade).GetDataType()
+        ncomp = output.GetArray(propiedade).GetNumberOfComponents()
+        print(tamaño)
+        if tipo == 11:
+            offset = offset_anterior +tamaño*8 + 4 # Hai catro bits que sobran entre cada propiedade
+            offset_anterior = offset
+            offsets.append(offset)
+        else:
+            offset = 0
+    return offsets
+
+def obter_raw_vtu(archivo):
+    # NON SEI SE ESTO FUNCONA SEMPRE!
+    # O ficheiro vtu ten unha cabeceira en xml e a continuación un bloque en binario
+    # As únicas líneas que parece que varía entre archivos son as de propiedades.
+    # Polo tanto, as líneas de cabeceira deben ser LINEAS_FIXAS + NUM_PROPIEDADES
+    # A maiores hai 3 líneas de texto (xml) ó final.
+    # LÍÑAS FIXAS = 20
+    liñas_fixas = 20
+    num_props = len(lista_vtu_props(abrir_vtu(archivo)))
+    liñas_texto = liñas_fixas + num_props
+
+    with open(archivo, 'rb') as f:
+        for line in range(liñas_texto):
+            f.readline()
+        raw = f.read()
+    # Non necesito chegar ata a final do archivo (creo), así que as líneas finais non
+    # estorban
+    return raw
+
 
 def leer_elements():
     # Código
@@ -47,4 +84,7 @@ def leer_nodes():
 
 if __name__ == "__main__":
     archivo = "../muestras/case0001.vtu"
-    leer_vtu(archivo)
+    obter_raw_vtu(archivo)
+    output = abrir_vtu(archivo)
+    lista_props = lista_vtu_props(output)
+    print(bin_offset_prop(output,lista_props))
